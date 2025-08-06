562fb6f5fbed59c7d359395d8be596412bc75076
RaviGupta965 <clicktoravi1234@gmail.com>
2025-08-06 12:07:57 +0530

    Tailwind-added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Register from './Auth/Register.jsx'
-import { Routes, Route} from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import Register from './Auth/Register.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
