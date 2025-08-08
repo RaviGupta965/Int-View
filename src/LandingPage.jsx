e1d2fbf9c7a4d6bc89f9c6f5151514fa032902f3
Arpan <arpanrohilla4@gmail.com>
2025-08-08 13:56:21 +0530

    Login enhance routing enabled

---
+++ src/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function LandingPage(){
+    return <section>
+        <h1>Landing Page</h1>
+        <button><a href="/login">Login</a></button>
+        <button><a href="/register">Register</a></button>
+    </section>
+}
+export default LandingPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import Register from './Auth/Register.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
