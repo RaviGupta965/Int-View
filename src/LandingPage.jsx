de2f2e317a621b9c76c0fa3288bb4b9463b88aba
RaviGupta965 <clicktoravi1234@gmail.com>
2025-08-10 18:56:34 +0530

    Deployment-issue-resolved

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { useNavigate,Link } from "react-router-dom";
 function LandingPage(){
+    const navigate= useNavigate();
     return <section>
         <h1>Landing Page</h1>
-        <button><a href="/login">Login</a></button>
-        <button><a href="/register">Register</a></button>
+        <Link to="/login"><button>Login</button></Link>
+        <Link to="/register"><button>Register</button></Link>
     </section>
 }
 export default LandingPage;
