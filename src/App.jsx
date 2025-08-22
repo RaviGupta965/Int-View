5df6d434a1d48ecc4fdb94373c666f41bbcd826a
Arpan <118041697+arpan2233@users.noreply.github.com>
2025-08-22 12:46:19 +0530

    Revert "Revert "Authorization enabled""

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './LandingPage'
-import Register from './Auth/Register'
-import Login from './Auth/Login'
 import Room from './RoomComponents/Room'
 import Meet from './Video/Meet'
+
+import {ClerkProvider, SignIn, SignUp} from '@clerk/clerk-react'
+// import App from './App.tsx'
+import './index.css'
+
+
 function App() {
-  return (<BrowserRouter>
-    <Routes>
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+  const CLERK_SIGN_IN_FORCE_REDIRECT_URL = import.meta.env.VITE_SIGN_IN_FORCE_REDIRECT_URL;
+  if (!PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key')
+  }
+  return <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<LandingPage/>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/room" element={<Room />} />
-        <Route path="/meet/:id" element={<Meet />} />
+        <Route path='/room' element={<Room/>}/>
+        <Route path='/meet/:id' element={<Meet/>}/>
       </Routes>
-  </BrowserRouter>);
+    </BrowserRouter>
+  </ClerkProvider>
 }
 
 export default App;
